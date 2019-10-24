be964396c0ab0806e6ea9f7c43b913e224c03db2
deathcod <crraksh@amazon.com>
2019-10-24 16:53:38 +0530

    Use provide module

--- module/AppModule.java
+++ module/AppModule.java
@@ -1,6 +1,8 @@
 package module;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 import annotations.ColorValue;
 import annotations.EdgeValue;
@@ -20,9 +22,24 @@ public class AppModule extends AbstractModule {
     protected void configure() {
         super.configure();
 
-        bind(DrawShape.class).to(DrawSquare.class);
+        //bind(DrawShape.class).to(DrawSquare.class);
         bind(Request.class).to(SquareRequest.class);
         bind(Integer.class).annotatedWith(EdgeValue.class).toInstance(40);
         bind(String.class).annotatedWith(ColorValue.class).toInstance("Red");
+    }
+
+
+    /**
+     * When a type is defined, and we require code to create object
+     * we create it using provide. This is the same implementation as
+     * opposed to bind module.
+     * 
+     * bind(DrawShape.class).to(DrawSquare.class)
+     * @return DrawShape
+     */
+    @Provides
+    @Singleton
+    DrawShape getDrawShape() {
+        return new DrawSquare("Blue", 40);
     }
 }
