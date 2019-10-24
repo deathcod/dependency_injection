805273b813a3d585e3a1b3a77bee5dde37f7f46e
deathcod <crraksh@amazon.com>
2019-10-24 14:07:33 +0530

    Create annotations

--- module/AppModule.java
+++ module/AppModule.java
@@ -2,6 +2,8 @@ package module;
 
 import com.google.inject.AbstractModule;
 
+import annotations.ColorValue;
+import annotations.EdgeValue;
 import request.Request;
 import request.SquareRequest;
 import service.DrawShape;
@@ -20,5 +22,7 @@ public class AppModule extends AbstractModule {
 
         bind(DrawShape.class).to(DrawSquare.class);
         bind(Request.class).to(SquareRequest.class);
+        bind(Integer.class).annotatedWith(EdgeValue.class).toInstance(40);
+        bind(String.class).annotatedWith(ColorValue.class).toInstance("Red");
     }
 }
--- service/DrawSquare.java
+++ service/DrawSquare.java
@@ -1,5 +1,10 @@
 package service;
 
+import com.google.inject.Inject;
+
+import annotations.ColorValue;
+import annotations.EdgeValue;
+
 /**
  * Draw square is an implementation to draw square shape.
  * 
@@ -7,9 +12,19 @@ package service;
  */
 public class DrawSquare implements DrawShape {
 
+    private final String color;
+    private final Integer edge;
+    private final String SQUARE_VALUE = "Draw square!!, with edge: %s with color: %s";
+
+    @Inject
+    public DrawSquare(@ColorValue String color, @EdgeValue Integer edge) {
+        this.color = color;
+        this.edge = edge;
+    }
+
     @Override
     public void draw() {
-        System.out.println("Draw square!!");
+        System.out.println(String.format(SQUARE_VALUE, edge, color));
     }
 
 }
